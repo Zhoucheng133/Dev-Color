34af1af08858b609fb6f736c6b184eff49b7d218
zhoucheng133 <zhoucheng133@outlook.com>
2024-03-03 19:02:49 +0800

    完成参数设定

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -2,11 +2,31 @@
   <div class="titleBar"></div>
   <div class="content">
     <div class="leftSide">
-      <div class="colorCircle"></div>
+      <div class="colorCircle" :style="{'background-color': colorGet}"></div>
     </div>
-    <div class="rightSide"></div>
+    <div class="rightSide">
+
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    colorGet(){
+      return `rgba(${this.colorR}, ${this.colorG}, ${this.colorB}, ${this.colorAlpha})`
+    }
+  },
+  data() {
+    return {
+      colorR: 255,
+      colorG: 255,
+      colorB: 255,
+      colorAlpha: 1,
+    }
+  },
+}
+</script>
 
 <style>
 body{
@@ -20,6 +40,7 @@ body{
   width: 120px;
   box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
   border-radius: 60px;
+  transition: background-color .2s linear;
 }
 .rightSide{
   height: calc(100vh - 60px);
