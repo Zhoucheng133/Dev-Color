206e1aba0c1115c691dcb5dc54799947be875100
zhoucheng133 <zhoucheng133@outlook.com>
2024-03-04 17:42:52 +0800

    添加显示代码

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -10,13 +10,25 @@
     </div>
     <div class="rightSide">
       <div class="inputArea">
+        <a-select v-model:value="selectLan" style="width: 150px;">
+          <a-select-option value="CSS">CSS</a-select-option>
+          <a-select-option value="Flutter">Flutter</a-select-option>
+        </a-select>
+
+        <div class="code">
+          <div>{{ codeContent() }}</div>
+          <div class="copyIcon">
+            <CopyOutlined />
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { onMounted, ref, watch } from 'vue';
+import { ref, watch } from 'vue';
+import { CopyOutlined } from '@ant-design/icons-vue';
 
 var colorR=ref('255');
 var colorG=ref('255');
@@ -24,6 +36,16 @@ var colorB=ref('255');
 var colorAlpha=ref('1');
 
 var colorHEX=ref('#FFFFFF')
+
+var selectLan=ref("CSS")
+
+function codeContent(){
+  if(selectLan.value=="CSS"){
+    return `rgb(${colorR.value}, ${colorG.value}, ${colorB.value})`
+  }else if(selectLan.value=="Flutter"){
+    return `Color.fromRGB(${colorR.value}, ${colorG.value}, ${colorB.value})`
+  }
+}
 
 function selectColor(){
   document.getElementById("colorPicker").click();
@@ -59,6 +81,26 @@ body{
 </style>
 
 <style scoped>
+.copyIcon:hover{
+  border: 1px solid black;
+}
+.copyIcon{
+  margin-left: auto;
+  padding: 5px 5px 5px 5px;
+  border: 1px solid rgb(240, 240, 240);
+  transition: border .2s linear;
+  cursor: pointer;
+  border-radius: 5px;
+}
+.code{
+  margin-top: 30px;
+  background-color: rgb(240, 240, 240);
+  padding: 15px 10px 15px 10px;
+  display: flex;
+  align-items: center;
+  border-radius: 10px;
+  color: rgb(70, 70, 70);
+}
 .fixColorPicker{
   position: absolute;
   left: 20px;
