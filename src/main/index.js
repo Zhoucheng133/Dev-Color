7383a77e27466e1f14510efbca9144f4a02c8482
zhoucheng133 <zhoucheng133@outlook.com>
2024-03-05 17:16:45 +0800

    删除不需要的代码

--- src/main/index.js
+++ src/main/index.js
@@ -4,7 +4,6 @@ import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
 
 function createWindow() {
-  const path = require('path');
   const mainWindow = new BrowserWindow({
     width: 600,
     height: 450,
